bc243ce8d99668f11d8d09abf76883d081eb8c4d
Victor Henrique Silva <henrictor@hotmail.com>
2021-11-02 13:49:50 -0300

    Delete dbengine directory

--- dbengine/dbconfig.py
+++
@@ -1,6 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from main import app
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mariadb+pymysql://root:94082@localhost/Genius_API'
-db = SQLAlchemy(app)
