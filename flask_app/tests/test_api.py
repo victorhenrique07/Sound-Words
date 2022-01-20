27983f1bf7c8f3615c4648319ac9d7f5bde1f632
Victor Henrique Silva <henrictor@hotmail.com>
2022-01-20 13:26:07 -0300

    added test to assert if register_artist is working

--- flask_app/tests/test_api.py
+++ flask_app/tests/test_api.py
@@ -2,6 +2,12 @@ import json
 
 from flask import Flask
 from flask_app.routes.routes import configure_routes
+from dotenv import dotenv_values
+from flask_app.config import db
+
+env = dotenv_values(".env")
+
+DATABASE_URI = env.get("DATABASE_URI")
 
 
 def test_server_is_running():
@@ -12,3 +18,22 @@ def test_server_is_running():
 
     response = client.get(url)
     assert response.status_code == 200
+
+
+def test_endpoint_post_home_register_artist_is_ok():
+    app = Flask(__name__)
+    configure_routes(app)
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    client = app.test_client()
+    url = '/home/register-artist'
+    data = {
+        "name": "Duzz",
+        "genre": "Trap"
+    }
+
+    response = client.post(url, json=data)
+    assert response.status_code == 201 or response.status_code == 500
+
+
