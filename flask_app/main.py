8cbfb8406a7fbe10af9b71aa4eb605b29da79fb3
Victor Henrique Silva <henrictor@hotmail.com>
2022-01-25 15:33:30 -0300

    adding db url

--- flask_app/main.py
+++ flask_app/main.py
@@ -10,7 +10,9 @@ app = Flask(__name__)
 
 configure_routes(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://sjjpjogjfvelel" \
+                                        ":ef4a9f37828c1fb02ebb6bfc1c1b7fbc9c4f5e9befe7ca6644f8d1c8ca3e51d8@ec2-3-216" \
+                                        "-113-109.compute-1.amazonaws.com:5432/d5rcanjcc13umr "
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
